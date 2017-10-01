94b30b3756e29a07576a9931282159544c306e9b
CMD <mac@JeongKee-MacBook-Pro.local>
2017-10-01 17:07:15 +0900

    String and Numbers

---
+++ Basic grammar/String and Numbers.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let name:String = "GconMaster"
+
+var greeting = "Hello"
+
+greeting += " " + name
+
+let characters = name.characters
+let count = characters.count
+
+let url = "gconmaster.github.io"
+let hasProtocol = url.hasPrefix("http://")
+
+print("\(name)")
+
+let maxSpeed:Int = 200
+//maxSpeed += 10
+
+var currentSpeed:Int = 110
+currentSpeed += 10
+currentSpeed += Int(20.5)
+
+let intMax = Int.max
+let UintMax = UInt.max
+let intMin = Int.min
+let UintMin = UInt.min
+
+let pi = 3.14
+let divider = 2
+let halfPi = 3.14/Double(divider)
+
